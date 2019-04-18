ed5bc8e4c71ac368897f18c5e5978f7511973ec3
Veronika <itmeans42@gmail.com>
2019-04-18 21:48:17 +0300

    Это мой первый коммит

---
+++ lesson_1/script.js
@@ -0,0 +1,28 @@
+let money = prompt ("Ваш бюджет на месяц?");
+
+let time = prompt ("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце:");
+let secondQuestion = prompt("Во сколько обойдется?");
+
+appData.expenses[firstQuestion] = secondQuestion;
+appData.expenses[firstQuestion] = secondQuestion;
+
+firstQuestion = prompt("Введите обязательную статью расходов в этом месяце:");
+secondQuestion = prompt("Во сколько обойдется?");
+
+appData.expenses[firstQuestion] = secondQuestion;
+appData.expenses[firstQuestion] = secondQuestion;
+
+alert(appData.budget / 30);
+
+console.log(appData);
