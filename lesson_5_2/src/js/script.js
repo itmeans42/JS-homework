0a73f9a5bc0cdfa946af1289febadf44f6d228fa
Veronika <itmeans42@gmail.com>
2019-04-29 08:13:13 +0300

    Выполнено задание к уроку №5

--- lesson_5_2/src/js/script.js
+++ lesson_5_2/src/js/script.js
@@ -1,45 +1,22 @@
 let startBtn = document.getElementById('start'),
-    budget = document.getElementsByClassName('budget-value'),
-    dayBudget = document.getElementsByClassName('daybudget-value'),
-    levelValue = document.getElementsByClassName('level-value'),
-    expensesValue = document.getElementsByClassName('expenses-value'),
-    optionalExpenses = document.getElementsByClassName('optionalexpenses-value'),
-    incomeValue = document.getElementsByClassName('income-value'),
-    monthSavings = document.getElementsByClassName('monthsavings-value'),
-    yearSavings = document.getElementsByClassName('yearsavings-value'),
-    yearValue = document.getElementsByClassName('year-value'),
-    monthValue = document.getElementsByClassName('month-value'),
-    dayValue = document.getElementsByClassName('day-value'),
-    expensesItemInput = document.getElementsByClassName('expenses-item'),
-    buttons = document.getElementsByTagName('button'),
-    optionalExpensesInput = document.querySelectorAll('.optionalexpenses'),
-    chooseIncome = document.querySelector('choose-income'),
-    checkSavings = document.querySelector('.checksavings'),
-    sum = document.querySelector('.sum'),
-    percent = document.querySelector('.percent'),
-    year = document.querySelectorAll('.year-value'),
-    month = document.querySelectorAll('.month-value'),
-    day = document.querySelectorAll('.day-value');
+    budget = document.getElementsByClassName('budget-value') [0],
+    dayBudget = document.getElementsByClassName('daybudget-value') [0],
+    levelValue = document.getElementsByClassName('level-value') [0],
+    expensesValue = document.getElementsByClassName('expenses-value') [0],
+    optionalExpenses = document.getElementsByClassName('optionalexpenses-value') [0],
+    incomeValue = document.getElementsByClassName('income-value') [0],
+    monthSavings = document.getElementsByClassName('monthsavings-value') [0],
+    yearSavings = document.getElementsByClassName('yearsavings-value') [0],
 
-console.log(startBtn);
-console.log(budget);
-console.log(dayBudget);
-console.log(levelValue);
-console.log(expensesValue);
-console.log(optionalExpenses);
-console.log(incomeValue);
-console.log(monthSavings);
-console.log(yearSavings);
-console.log(yearValue);
-console.log(monthValue);
-console.log(dayValue);
-console.log(expensesItemInput);
-console.log(buttons);
-console.log(optionalExpensesInput);
-console.log(chooseIncome);
-console.log(checkSavings);
-console.log(sum);
-console.log(percent);
-console.log(year);
-console.log(month);
-console.log(day);
+    expensesItem= document.getElementsByClassName('expenses-item'),    
+    expensesBtn = document.getElementsByTagName('button') [0],
+    optionalExpensesBtn = document.getElementsByTagName('button') [1],
+    countBtn = document.getElementsByTagName('button') [2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
