229430be725558a958195b4ba7cdc5fd2a793e37
Veronika <itmeans42@gmail.com>
2019-05-18 08:03:47 +0300

    Выполнено задание к уроку №14

---
+++ lesson_14/src/js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+	$('[href="#sheldure"]').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.main_btn').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.main_btna').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.close').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideUp("slow");
+	});
+});
